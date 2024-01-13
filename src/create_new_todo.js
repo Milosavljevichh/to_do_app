fb95cf206db26e218ab8e109c5c248fbf3327781
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-01-13 22:01:31 +0100

    added saving todos to local storage

--- src/create_new_todo.js
+++ src/create_new_todo.js
@@ -1,17 +1,20 @@
 import add_todo_to_DOM from "./add-todo-to-DOM";
 
-export default function create_new_todo(title, description, due_date, priority, in_category) {
+export default function create_new_todo(title, description, due_date, priority) {
 
     class Todo {
-        constructor(title, description, due_date, priority, in_category) {
+        constructor(title, description, due_date, priority) {
             this.title = title;
             this.description = description;
             this.due_date = due_date;
             this.priority = priority
-            this.in_category = in_category
+            this.in_category = document.getElementById('workspace-header').querySelector('h1').innerHTML;
         }
     }
 
-    const new_todo = new Todo(title.value, description.value, due_date.value, priority.value, in_category);
+    const new_todo = new Todo(title.value, description.value, due_date.value, priority.value);
+
+    localStorage.setItem('todo_'+title.value, JSON.stringify(new_todo))
+
     add_todo_to_DOM(new_todo);
 };
--- src/index.js
+++ src/index.js
@@ -7,17 +7,16 @@ import create_new_todo from "./create_new_todo";
  const description = document.getElementById('todo-description');
  const due_date = document.getElementById('todo-dueDate');
  const priority = document.getElementById('todo-priority');
- const in_category = document.getElementById('workspace-header').querySelector('h1').innerHTML;
-
+ 
  const new_todo_btn = document.getElementById('add-new-todo');
-
+ 
 add_project.addEventListener('click', ()=>{
     const new_project_title = title_input.value;
     create_new_project(new_project_title);
 });
 
-new_todo_btn.addEventListener('click',  ()=>{
-    create_new_todo(title, description, due_date, priority, in_category);
-})
-
 create_new_project('default');
+
+new_todo_btn.addEventListener('click',  ()=>{
+    create_new_todo(title, description, due_date, priority);
+})
