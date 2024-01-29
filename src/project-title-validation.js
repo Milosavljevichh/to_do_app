2ab9aaa3e49d0f5a3f90c0fed7d78fdfab648320
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-01-29 22:16:13 +0100

    project now can't be created if a project with the same name exists

--- src/project-title-validation.js
+++ src/project-title-validation.js
@@ -2,8 +2,16 @@ export default function validateProjectTitle() {
     const add_project = document.getElementById('add-project-title');
     const title_input = document.getElementById('project-title');
     let title = title_input.value;
+    let projectExists = false;
 
-    if (title.length < 2) {
+    Object.keys(localStorage).forEach(key => {
+        if (key === 'project_'+title) {
+            projectExists = true;
+            console.log(title)
+        }
+      });
+
+    if (title.length < 2 || projectExists === true) {
         add_project.style.display = 'none';
     } else {
         add_project.removeAttribute('style')
